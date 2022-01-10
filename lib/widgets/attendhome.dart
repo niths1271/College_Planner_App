429d36ab19e232108e5a21d029b81332385358d5
noorfathimaarfa <71307499+noorfathimaarfa@users.noreply.github.com>
2022-01-10 09:09:48 +0530

    Update attendhome.dart

--- lib/widgets/attendhome.dart
+++ lib/widgets/attendhome.dart
@@ -1,152 +1,321 @@
 
 
-import 'package:college_planner_app/widgets/attendence.dart';
+import 'package:college_planner_app/pages/attendence.dart';
 import 'package:college_planner_app/widgets/date_picker_widget.dart';
-import 'package:college_planner_app/widgets/grades.dart';
-
+import 'package:college_planner_app/pages/grades.dart';
+import 'agendaq.dart';
 import 'package:college_planner_app/widgets/button_widget.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
+import 'package:date_format/date_format.dart';
 import 'package:intl/intl.dart';
 import 'package:college_planner_app/widgets/time_picker_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
+final _firestore = FirebaseFirestore.instance;
+late User loggedInUser;
 
-
-class Date extends StatelessWidget {
-  static final String title = 'Date (Range) & Time';
-//  Date({required this.DatePickerWidget(),required this.TimePickerWidget()()})
-
+class AttendenceAdd extends StatefulWidget {
   @override
-  Widget build(BuildContext context) => MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: title,
-    theme: ThemeData(
-      primaryColor: Colors.black,
-    ),
-    home: MainPage(),
-  );
+  _AttendenceAddState createState() => _AttendenceAddState();
 }
 
-class MainPage extends StatefulWidget {
-  @override
-  _MainPageState createState() => _MainPageState();
-}
+class _AttendenceAddState extends State<AttendenceAdd> {
+  // start datetime
 
-class _MainPageState extends State<MainPage> {
+  late double _height;
+  late double _width;
+
+  late String _setTime, _setDate;
+
+  late String _hour, _minute, _time;
+
+  late String dateTime;
+
+  DateTime selectedDate = DateTime.now();
+
+  TimeOfDay selectedTime = TimeOfDay(hour: 00, minute: 00);
+
+  TextEditingController _dateController = TextEditingController();
+  TextEditingController _timeController = TextEditingController();
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        initialDatePickerMode: DatePickerMode.day,
+        firstDate: DateTime(2015),
+        lastDate: DateTime(2101));
+    if (picked != null)
+      setState(() {
+        selectedDate = picked;
+        _dateController.text = DateFormat.yMd().format(selectedDate);
+      });
+  }
+
+  Future<Null> _selectTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: selectedTime,
+    );
+    if (picked != null)
+      setState(() {
+        selectedTime = picked;
+        _hour = selectedTime.hour.toString();
+        _minute = selectedTime.minute.toString();
+        _time = _hour + ' : ' + _minute;
+        _timeController.text = _time;
+        _timeController.text = formatDate(
+            DateTime(2019, 08, 1, selectedTime.hour, selectedTime.minute),
+            [hh, ':', nn, " ", am]).toString();
+      });
+  }
+
+  // end datetime
+  final _auth = FirebaseAuth.instance;
+
+  void getC() async {
+    try {
+      final user = await _auth.currentUser!;
+      if (user != null) {
+        loggedInUser = user;
+        print(user);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _dateController.text = DateFormat.yMd().format(DateTime.now());
+
+    _timeController.text = formatDate(
+        DateTime(2019, 08, 1, DateTime.now().hour, DateTime.now().minute),
+        [hh, ':', nn, " ", am]).toString();
+    super.initState();
+    getC();
+  }
+
   int index = 0;
+
   //String text;
-
-
-  //bool isOn = false;
-
-  bool _flutter=false;
-
- // String newDate;
+  late String  title;
+  late DateTime atTime;
 
 
 
+  bool _flutter = false;
 
 
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-
-
-
-
-
-
-
-    backgroundColor: Colors.white,
-    appBar: AppBar(
-      // Here we take the value from the MyHomePage object that was created by
-      // the App.build method, and use it to set our appbar title.
-      title: Text('Attendence'),
-      backgroundColor: Colors.blue,
-      automaticallyImplyLeading: true,
-      //`true` if you want Flutter to automatically add Back Button when needed,
-      //or `false` if you want to force your own back button every where
-      leading: IconButton(icon:Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) =>Attendence()),
-            );
-          }
-
-
-      ),
-      actions: <Widget>[
-        TextButton(
-            style: TextButton.styleFrom(
-           //   backgroundColor: Colors.blue.shade300,
-              //  padding: const EdgeInsets.all(16.0),
-              primary:Colors.white,
-              textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-
-            ),
+  Widget build(BuildContext context) {
+    _height = MediaQuery.of(context).size.height / 2;
+    _width = MediaQuery.of(context).size.width;
+    dateTime = DateFormat.yMd().format(DateTime.now());
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text('Attendence'),
+        backgroundColor: Colors.blue,
+        automaticallyImplyLeading: true,
+        //`true` if you want Flutter to automatically add Back Button when needed,
+        //or `false` if you want to force your own back button every where
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back),
             onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) =>Attendence()),
-              );
+              Navigator.pop(context);
+            }),
+        actions: <Widget>[
+          TextButton(
+            style: TextButton.styleFrom(
+              //   backgroundColor: Colors.blue.shade300,
+              //  padding: const EdgeInsets.all(16.0),
+              primary: Colors.white,
+              textStyle:
+              const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            onPressed: () async {
+              atTime = DateTime(selectedDate.year, selectedDate.month,
+                  selectedDate.day, selectedTime.hour, selectedTime.minute);
+              await _firestore.collection('Attendence').add({
+                'value': _flutter,
+                'title': title,
+                'sender': loggedInUser.uid,
+                'date': Timestamp.fromDate(atTime),
+              });
+              print("Details entered successfully");
+              Navigator.pop(context);
             },
             child: const Text("ADD"),
-
-        ),
-      ],
-    ),
-    body: Padding(
-
-      padding: EdgeInsets.all(32),
-
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-
-
-
-
-            // args = ModalRoute.of(context)!.settings.arguments as DatePickerWidget;
-        DatePickerWidget(),
-          const SizedBox(height: 10),
-          TimePickerWidget(),
-          const SizedBox(height: 10),
-
-
-          Column(
-            children: [
-              SwitchListTile(
-                title: Text('JUSTIFICATION',style: TextStyle(
-                    color: Colors.blue,
-                    fontWeight: FontWeight.w800,
-
-                    fontSize: 20
-                ),
-                ),
-                value: _flutter,
-                activeColor: Colors.lightBlue,
-                inactiveTrackColor: Colors.grey,
-                onChanged: (bool value) {
-                  setState(() {
-                    _flutter = value;
-                  });
-                },
-              ),
-
-            ],
           ),
-
         ],
       ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // args = ModalRoute.of(context)!.settings.arguments as DatePickerWidget;
+              Container(
+                width: _width / 1.7,
+                child: TextField(
+                  onChanged: (value) {
+                    title = value;
+                  },
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    labelText: "Title",
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              /*    Container(
+                width: _width / 1.7,
+                child: TextField(
+                  onChanged: (value) {
+                    note = value;
+                  },
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    labelText: "Note",
+                  ),
+                ),
+              ),*/
+              const SizedBox(height: 10),
+              // datetime start
+
+              Container(
+                width: _width,
+                height: _height,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Text(
+                          'Choose Date',
+                          style: TextStyle(
+                            // fontStyle: FontStyle.italic,
+                              fontWeight: FontWeight.w600,
+                              letterSpacing: 0.5),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            _selectDate(context);
+                          },
+                          child: Container(
+                            width: _width / 1.7,
+                            height: _height / 6,
+                            margin: EdgeInsets.only(top: 8.0),
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(color: Colors.grey[200]),
+                            child: TextFormField(
+                              style: TextStyle(fontSize: 40),
+                              textAlign: TextAlign.center,
+                              enabled: false,
+                              keyboardType: TextInputType.text,
+                              controller: _dateController,
+                              onSaved: (val) {
+                                _setDate = val.toString();
+                              },
+                              decoration: InputDecoration(
+                                  disabledBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide.none),
+                                  // labelText: 'Time',
+                                  contentPadding: EdgeInsets.only(top: 0.0)),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(
+                          'Choose Time',
+                          style: TextStyle(
+                            // fontStyle: FontStyle.italic,
+                              fontWeight: FontWeight.w600,
+                              letterSpacing: 0.5),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            _selectTime(context);
+                          },
+                          child: Container(
+                            margin: EdgeInsets.only(top: 8),
+                            width: _width / 1.7,
+                            height: _height / 6,
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(color: Colors.grey[200]),
+                            child: TextFormField(
+                              style: TextStyle(fontSize: 40),
+                              textAlign: TextAlign.center,
+                              onSaved: (val) {
+                                _setTime = val.toString();
+                              },
+                              enabled: false,
+                              keyboardType: TextInputType.text,
+                              controller: _timeController,
+                              decoration: InputDecoration(
+                                  disabledBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide.none),
+                                  // labelText: 'Time',
+                                  contentPadding: EdgeInsets.all(5)),
+                            ),
+                          ),
+
+                        ),
+
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Column(
+                children: [
+                  SwitchListTile(
+                    title: Text('JUSTIFICATION',style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.w800,
+
+                        fontSize: 20
+                    ),
+                    ),
+                    value: _flutter,
+                    activeColor: Colors.lightBlue,
+                    inactiveTrackColor: Colors.grey,
+                    onChanged: (bool value) {
+                      setState(() {
+                        _flutter = value;
+                      });
+                    },
+                  ),
+
+                ],
+              ),
 
 
-    ),
-  );
+              // datetime end
+            ],
+          ),
+        ),
+      ),
 
+    );
+  }
 }
 
+// waste
 
 
